85d9205ced06068fff68a699cc735511fc3fb214
mlsniper <mlsniper@163.com>
2020-02-16 22:19:56 +0800

    python code

---
+++ 1.24.3.py
@@ -0,0 +1,6 @@
+s = '''
+"Collusion is very real with Russia," Trump quoted conservative commentator Dan Bongino as saying on Trump's favorite Fox News morning show, "but only with Hillary and the Democrats, and we should demand a full investigation."
+'''
+s = s.replace(','," ")
+ls = s.split()
+print(len(s))
